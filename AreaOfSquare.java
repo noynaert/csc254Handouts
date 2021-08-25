a4eebd725d9f72dd66dbadd147cb5b952b590c15
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-08-25 13:42:35 -0500

    Continuing chapter 02

---
+++ AreaOfSquare.java
@@ -0,0 +1,19 @@
+
+
+public class AreaOfSquare {
+    public static void main(String[] args){
+        double length;   //The length of one side of the square
+        double area;     //The area of the square
+
+        // Get the input
+        length = 5.2;
+
+        // Computer the area
+        area = length * length;
+
+        //Display the results
+        System.out.println("A square which has a side " + length 
+                        +  " units has an area of " + area + " square units."
+        );
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+/*
+   Simple Hello, World program.
+*/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }    
+}
