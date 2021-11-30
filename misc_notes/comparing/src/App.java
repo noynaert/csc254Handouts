2285d994c311bc46a05d89c944b11956b911cc60
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-11-30 08:33:56 -0600

    implements sorting

--- misc_notes/comparing/src/App.java
+++ misc_notes/comparing/src/App.java
@@ -1,7 +1,31 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Person pn = new Person("Fred", 30);
+        String[] words = { "bob", "ted", "carole", "alice" };
         System.out.println(pn);
         System.out.println("Hello, World!");
+        printList(words);
+        Person[] people = { pn,
+                    new Person("Barney",29),
+                                        new Person("Wilma",31),
+
+                    new Person("Betty", 29)
+        };
+        printList(people);
+        Arrays.sort(words, 0, words.length);
+        printList(words);
+        Arrays.sort(people, 0, words.length);
+        printList(people);
+
+    }
+
+    public static void printList(Object[] list) {
+        System.out.println("---------------");
+        for (int i = 0; i < list.length; i++) {
+            System.out.printf("[%d] %s\n", i, list[i]);
+        }
+        System.out.println("");
     }
 }
--- misc_notes/comparing/src/Person.java
+++ misc_notes/comparing/src/Person.java
@@ -1,4 +1,4 @@
-public class Person {
+public class Person implements Comparable<Person>{
     private String name;
     private int age;
 
