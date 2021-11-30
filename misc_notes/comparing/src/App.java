7cfdf548cf0e7efea1970fa77ee8c99d16eb8b1a
J. Evan Noynaert <noynaert@missouriwestern.edu>
2021-11-30 08:08:03 -0600

    compareTo and other standard methods

---
+++ misc_notes/comparing/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person pn = new Person("Fred", 30);
+        System.out.println(pn);
+        System.out.println("Hello, World!");
+    }
+}
