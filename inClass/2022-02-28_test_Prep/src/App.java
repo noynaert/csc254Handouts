8945673867c7e1e7c78d9b6eced79bca5697a54d
J. Evan Noynaert <noynaert@missouriwestern.edu>
2022-03-07 07:30:18 -0600

    add in-class example

---
+++ inClass/2022-02-28_test_Prep/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        //2.3.1 How do you write a statement to let the user enter a double value from the ­keyboard? What happens if you entered 5a when executing the following code?
+
+        // Scanner input = new Scanner(System.in);
+        // double x = input.nextDouble();
+        // System.out.println(x);
+
+        // double radius = input.nextDouble();
+
+        int $thing = 5;
+        System.out.println($thing);
+
+    }
+}
